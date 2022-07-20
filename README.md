b1fb74a4758b7fcff92154ce86f4b350f438a489
Adrian Edelen <adrianedelen@gmail.com>
2022-07-20 16:06:10 -0400

    MEMORY

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # oxide
+
+
+“An idiot admires complexity, a genius admires simplicity, a physicist tries to make it simple, for an idiot anything the more complicated it is the more he will admire it, if you make something so clusterfucked he can't understand it he's gonna think you're a god cause you made it so complicated nobody can understand it. That's how they write journals in Academics, they try to make it so complicated people think you're a genius”
+
+― Terry Davis, Creator of Temple OS 
+
+Oxide has a few primary goals in mind. 
+    - simplicity, oxide is designed to be small and simple. the kernel is not meant to be this huge piece of code with support for anything that can be thrown at it. Instead, the kernel itself is designed to be able to be worked on by the user, tweaked and customized for the users hardware and use case. Don't need a filesystem, take it out (it might be running in userspace anyway)
+
+    - oxide is also designed with the idea of a single user environment. Modern desktop operating systems are still built off the back of server and mainframe systems. this doesn't make sense to me. when looking at any smart phone or mobile OS, there is typically one user, no sign in page, no user perms to deal with. oxide, most people use computers like this now. oxide is built from the ground up to be a single user experience. that isn't to say it can't be used by more people. but why add the overhead.
+
+
+
 Due to the nature of kernel development, there are some odd steps that must be taken in order to build the project
 
 Additionally, for testing, it is Highly recommended that Qemu is used with this configuration.
--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,13 @@
 use x86_64::{ structures::paging::PageTable, VirtAddr, };
-use x86_64::PhysAddr;
+use x86_64::{PhysAddr, structures::paging::{Page, PhysFrame, Mapper, Size4KiB, FrameAllocator,}};
+use x86_64::structures::paging::OffsetPageTable;
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+
 
 /// Returns a mutable reference to the active level 4 table.
 ///
@@ -7,7 +15,7 @@ use x86_64::PhysAddr;
 /// complete physical memory is mapped to virtual memory at the passed
 /// `physical_memory_offset`. Also, this function must be only called once
 /// to avoid aliasing `&mut` references (which is undefined behavior).
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
     let (level_4_table_frame, _) = Cr3::read();
     let phys = level_4_table_frame.start_address();
@@ -52,6 +60,28 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
 
     }
 
+    // calculate the physical address by adding the page offset
     Some(frame.start_address() + u64::from(addr.page_offset()))
 
 }
+
+pub fn create_example_mapping(page: Page, mapper: &mut OffsetPageTable, frame_allocator: &mut impl FrameAllocator<Size4KiB>,) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+
